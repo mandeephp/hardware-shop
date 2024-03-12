881be4f0d83a43b3353e15708eee525b71ee5a4b
Mandeep <mandeepthakurhp@gmail.com>
2024-03-12 13:28:49 +0530

    Updated allowed hosts

--- hardware_shop/settings.py
+++ hardware_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7bocg&uiy74*&4p2-ki-2+#l)gekiti7+15)#jis_q=m8rsfy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
